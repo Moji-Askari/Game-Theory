a4d9d4a57ff04f2117c14f2dbc07287098a3cd26
M. Askari <90698678+Moji-Askari@users.noreply.github.com>
2023-12-18 12:12:47 +0330

    Rock-paper-scissors game - solve with Nashpy

---
+++ nashpycode.py
@@ -0,0 +1,18 @@
+import nashpy as nash
+import numpy as np
+
+A = np.array([[0, -1, 1], [1, 0, -1], [-1, 1, 0]])
+B = - A
+
+rps = nash.Game(A, B)
+print(rps)
+
+equilibria = rps.support_enumeration()
+
+for eq in equilibria:
+    print("Nash Equilibrium:", eq) 
+
+eq_strategy_player1, eq_strategy_player2 = list(rps.support_enumeration())[0]
+
+print ("eq_strategy_player1:", eq_strategy_player1)
+print ("eq_strategy_player2:", eq_strategy_player2)
